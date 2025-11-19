6188ab7bbf2a52a83483b9e4e5736020286b3e13
Olivier Maréchal <om@taktik.com>
2025-11-19 10:57:55 +0100

    clean

--- src/core/hooks/usePermissions.ts
+++ src/core/hooks/usePermissions.ts
@@ -11,26 +11,21 @@ export const usePermissions = (skipProp: boolean = false) => {
   const isAdministrator = useMemo(() => dataOwnerInfo?.role === UserRole.ADMINISTRATOR, [dataOwnerInfo])
   const isHeadOfService = useMemo(() => dataOwnerInfo?.role === UserRole.HEAD_OF_SERVICE, [dataOwnerInfo])
   const isCityWorker = useMemo(() => dataOwnerInfo?.role === UserRole.CITY_WORKER, [dataOwnerInfo])
-
   const isAdminLevel = useMemo(() => isAdministrator || isHeadOfService, [isAdministrator, isHeadOfService])
 
   const attachedService = useMemo(() => {
     const dataOwner = dataOwnerInfo?.dataOwner
-
     if (dataOwner && 'supervisorId' in dataOwner) {
       return dataOwner.supervisorId
     }
-
     return undefined
   }, [dataOwnerInfo])
 
   const attachedSite = useMemo(() => {
     const dataOwner = dataOwnerInfo?.dataOwner
-
     if (dataOwner && 'parentId' in dataOwner && (isHeadOfService || isCityWorker)) {
       return dataOwner.parentId
     }
-
     return undefined
   }, [dataOwnerInfo, isHeadOfService, isCityWorker])
 
